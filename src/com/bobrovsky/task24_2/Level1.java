47fc2f5307a1922637aa7aadb510f65234427503
Sergey Igoshin <macinq@gmail.com>
2020-06-06 16:54:28 +0400

    Task 24_2 fixed

--- src/com/bobrovsky/task24_2/Level1.java
+++ src/com/bobrovsky/task24_2/Level1.java
@@ -5,8 +5,13 @@ import java.util.*;
 public class Level1 {
     public static int odometer(int[] oksana) {
         int distance = 0;
-        for (int i = 0; i < oksana.length; i += 2) {
-            distance += oksana[i];
+        int time = oksana[1];
+        distance += oksana[0] * time;
+        for (int i = 0; i < oksana.length; i++) {
+            if (i > 1 && i % 2 != 0) {
+                time = oksana[i] - oksana[i - 2];
+                distance += oksana[i - 1] * time;
+            }
         }
         return distance;
     }
