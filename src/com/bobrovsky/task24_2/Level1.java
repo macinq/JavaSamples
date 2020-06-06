211a191b85cbab782b43ed8efc1cf9ec544d1a82
Sergey Igoshin <macinq@gmail.com>
2020-06-06 16:59:27 +0400

    Task 24_2 fixed 2

--- src/com/bobrovsky/task24_2/Level1.java
+++ src/com/bobrovsky/task24_2/Level1.java
@@ -7,8 +7,8 @@ public class Level1 {
         int distance = 0;
         int time = oksana[1];
         distance += oksana[0] * time;
-        for (int i = 0; i < oksana.length; i++) {
-            if (i > 1 && i % 2 != 0) {
+        for (int i = 2; i < oksana.length; i++) {
+            if (i % 2 != 0) {
                 time = oksana[i] - oksana[i - 2];
                 distance += oksana[i - 1] * time;
             }
