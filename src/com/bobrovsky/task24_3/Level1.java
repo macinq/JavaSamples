dfe4f9867443603dbcd1c972dc1794a874801982
Sergey Igoshin <macinq@gmail.com>
2020-06-10 15:53:37 +0400

    Task 24_3 completed

---
+++ src/com/bobrovsky/task24_3/Level1.java
@@ -0,0 +1,82 @@
+package com.bobrovsky.task24_3;
+
+public class Level1 {
+  public static int ConquestCampaign(int N, int M, int L, int[] battalion) {
+    int days = 0;
+    int[][] area = new int[N][M];
+    int flag = 0;
+
+    for (int i = 0; i < N; i++) {
+      for (int j = 0; j < M; j++) {
+        for (int z = 0; z < battalion.length; z += 2) {
+          area[battalion[z] - 1][battalion[z + 1] - 1] = 2;
+        }
+      }
+    }
+    days++;
+
+    while (flag != 1) {
+      for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+          if (area[i][j] == 2) {
+            if ((i >= 1 && i < N - 1) && (j >= 1 && j < M - 1)) {
+              area[i - 1][j] += 1;
+              area[i + 1][j] += 1;
+              area[i][j - 1] += 1;
+              area[i][j + 1] += 1;
+            }
+
+            if (i == N - 1 && j == M - 1) {
+              area[i - 1][j] += 1;
+              area[i][j - 1] += 1;
+            } else if (i == 0 && j == 0) {
+              area[i + 1][j] += 1;
+              area[i][j + 1] += 1;
+            } else if (i == 0 && j == M - 1) {
+              area[i + 1][j] += 1;
+              area[i][j - 1] += 1;
+            } else if (i == N - 1 && j == 0) {
+              area[i - 1][j] += 1;
+              area[i][j + 1] += 1;
+            }
+
+            if ((i >= 1 && i < N - 1) && j == 0) {
+              area[i - 1][j] += 1;
+              area[i + 1][j] += 1;
+              area[i][j + 1] += 1;
+            }
+            if ((i >= 1 && i < N - 1) && j == M - 1) {
+              area[i - 1][j] += 1;
+              area[i + 1][j] += 1;
+              area[i][j - 1] += 1;
+            }
+            if (i == 0 && (j >= 1 && j < M - 1)) {
+              area[i][j - 1] += 1;
+              area[i][j + 1] += 1;
+              area[i + 1][j] += 1;
+            }
+            if (i == N - 1 && (j >= 1 && j < M - 1)) {
+              area[i][j - 1] += 1;
+              area[i][j + 1] += 1;
+              area[i - 1][j] += 1;
+            }
+          }
+        }
+      }
+      days++;
+
+      for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+          if (area[i][j] == 0) {
+            flag = 0;
+            break;
+          } else {
+            flag = 1;
+          }
+        }
+      }
+    }
+
+    return days;
+  }
+}
